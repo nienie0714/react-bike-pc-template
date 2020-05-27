f39283f3dd0a8bddff00e7ccf3c789abf1ddd23d
home_wuhao <yy_wuhao@163.com>
2020-05-27 15:52:48 +0800

    less less-loader

--- src/pages/demo/Life.js
+++ src/pages/demo/Life.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import Child from './Child'
-import './index.css'
+import React from 'react';
+import Child from './Child';
+import './index.less';
 
 export default class Life extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         count: 0
-    //     }
-    // }
+  // constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //         count: 0
+  //     }
+  // }
 
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  };
 
-    handleAdd=()=>{
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+  handleAdd = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
 
-    handleClick() {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
 
-    render() {
-        // return <div style={{padding:50}}>
-        return <div className="content">
-            <p>React生命周期介绍</p>
-            <button onClick={this.handleAdd}>点击一下</button>
-            <button onClick={this.handleClick.bind(this)}>点击一下</button>
-            <p>{this.state.count}</p>
-            <Child name={this.state.count}></Child>
-        </div>
-    }
+  render() {
+    // return <div style={{padding:50}}>
+    return (
+      <div className="content">
+        <p>React生命周期介绍</p>
+        <button onClick={this.handleAdd}>点击一下</button>
+        <button onClick={this.handleClick.bind(this)}>点击一下</button>
+        <p>{this.state.count}</p>
+        <Child name={this.state.count}></Child>
+      </div>
+    );
+  }
 }
